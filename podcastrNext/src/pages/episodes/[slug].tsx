d8f466790899ce8c8a030f7ca218bca6f907c032
Chalps <pedrochamar12@gmail.com>
2021-04-22 16:48:48 -0300

    Dia03 - NLW

--- podcastrNext/src/pages/episodes/[slug].tsx
+++ podcastrNext/src/pages/episodes/[slug].tsx
@@ -2,8 +2,12 @@ import { GetStaticPaths, GetStaticProps } from "next";
 import { useRouter } from "next/router";
 import { api } from "../../services/api";
 import { format, parseISO } from "date-fns";
+import Link from "next/Link";
 import ptBR from "date-fns";
+import Image from "next/image";
 import { convertDurationToTimeString } from "../../utils/convertDurationToTimeString";
+
+import styles from "./episode.module.scss";
 
 type Episode = {
   id: string;
@@ -22,7 +26,38 @@ type EpisodeProps = {
 };
 
 export default function Episode({ episode }: EpisodeProps) {
-  return <h1>{episode.title}</h1>;
+  return (
+    <div className={styles.episode}>
+      <div className={styles.thumbnailContainer}>
+        <Link href="/">
+          <button type="button">
+            <img src="/arrow-left.svg" alt="Voltar" />
+          </button>
+        </Link>
+        <Image
+          width={700}
+          height={160}
+          src={episode.thumbnail}
+          objectFit="cover"
+        />
+        <button type="button">
+          <img src="/play.svg" alt="Tocar episódio" />
+        </button>
+      </div>
+
+      <header>
+        <h1>{episode.title}</h1>
+        <span>{episode.members}</span>
+        <span>{episode.publishedAt}</span>
+        <span>{episode.durationAsString}</span>
+      </header>
+
+      <div
+        className={styles.description}
+        dangerouslySetInnerHTML={{ __html: episode.description }}
+      />
+    </div>
+  );
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
